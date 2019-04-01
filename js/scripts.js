dee042eb082f7c8d87117c93d87e1509bcbedb28
zsuzsannamangu <zsuzsannamangu@gmail.com>
2019-04-01 12:03:44 -0700

    write basic html and css for form, add javascript looping

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+  $("button").click(function(event){
+
+    var blanks = ["movie", "book", "animal", "color"];
+    blanks.forEach(function(blank) {
+      var userInput = $("input#" + blank).val();
+      $("." + blank).text(userInput);
+    });
+
+    $(".answer").show();
+
+  event.preventDefault();
+  });
+});
+
+
+
+// var movieInput = $("input#movie").val();
+// var bookInput = $("input#book").val();
+// var animalInput = $("input#animal").val();
+// var colorInput = $("input#color").val();
+//
+// var formInput = $("input#form, ").val();
+//
+// $(".movie").text(movieInput);
+// $(".book").text(bookInput);
+// $(".animal").text(animalInput);
+// $(".color").text(colorInput);
